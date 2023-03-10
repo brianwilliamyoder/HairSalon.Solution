650062dbefebdcf7da3f47237375c0c98d576def
Brian Yoder <brianyoder@gmail.com>
2023-03-10 11:56:41 -0800

    add displayname attribute

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace HairSalon.Models
 {
   public class Stylist
   {
+    
     public int StylistId { get; set; }
+    [DisplayName("Enter the name of the stylist:")]
     public string StylistName { get; set; }
     public List<Client> Items { get; set; }
   }
