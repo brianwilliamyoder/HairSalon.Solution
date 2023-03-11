805ec0fff97fad85d75ddda54088564dd808ff57
Brian Yoder <brianyoder@gmail.com>
2023-03-11 10:02:13 -0800

    add DisplayName attritube to ClientName

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -1,10 +1,13 @@
+using System.ComponentModel;
+
 namespace HairSalon.Models
 {
   public class Client
   {
     public int StylistId { get; set; }
-    public Stylist stylist { get; set; }
+    public Stylist Stylist { get; set; }
     public int ClientId { get; set; }
+    [DisplayName("Enter Client's Name:")]
     public string ClientName { get; set; }
   }
 }
