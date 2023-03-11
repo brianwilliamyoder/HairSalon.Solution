e711acaf304f7b17ecc00f4b000b4caf59f32a0b
Brian Yoder <brianyoder@gmail.com>
2023-03-11 10:01:52 -0800

    create index, create, and details view

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -18,9 +18,38 @@ namespace HairSalon.Controllers
       _db = db;
     }
 
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients
+                              .Include(client => client.Stylist)
+                              .ToList();        
+      return View(model);
+    }
+
     public ActionResult Create()
     {
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "StylistName");
       return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      if (client.StylistId == 0)
+      {
+        return RedirectToAction("Create");
+      }
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    
+    public ActionResult Details(int id)
+    {
+      Client thisClient = _db.Clients
+                      .Include(client => client.Stylist)
+                      .FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
     }
   }
 }
