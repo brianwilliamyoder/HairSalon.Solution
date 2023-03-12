cf505dd12fc52de9e033d6806ade817232a0a5de
Brian Yoder <brianyoder@gmail.com>
2023-03-11 20:43:45 -0800

    add edit route to stylists controller

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -41,6 +41,12 @@ namespace HairSalon.Controllers
                       .FirstOrDefault(stylist => stylist.StylistId == id);
       return View(thisStylist);
     }
+
+    public ActionResult Edit(int id)
+    {
+      Stylist thisStylist = _db.Stylists.FirstOrDefault(Stylist => Stylist.StylistId == id);
+      return View(thisStylist);
+    }
   }
     
 }
